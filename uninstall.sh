ef3261add7f8923ffcbff2622cafc8055705fe51
zhixingfeng <zhixingfeng@192.168.2.4>
2015-12-31 00:31:18 -0500

    add BamFilter

--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,6 @@
-rm -rf third_party/local
+rm -rf third_party/src/bamtools/build
+rm -rf third_party/src/bamtools/bin
+rm -rf third_party/src/bamtools/include
+rm -rf third_party/src/bamtools/lib
+
 
